4db18a32d77255e77b29453f138da362fe120c31
Roland Martin <mail@rolandmartin.org>
2009-10-30 17:08:09 +0100

    -bres_mat contains resolution matrix calculation

---
+++ bres_mat.for
@@ -0,0 +1,30 @@
+      SUBROUTINE bres_mat
+      
+      USE alloci
+      
+      IMPLICIT none
+      
+      INCLUDE 'parmax.fin'
+      INCLUDE 'elem.fin'
+      INCLUDE 'dat.fin'
+      INCLUDE 'model.fin'
+      INCLUDE 'fem.fin'
+      INCLUDE 'inv.fin'
+      INCLUDE 'konv.fin'
+
+
+!     help variable  
+      INTEGER :: i,j,k,l
+
+      
+      IF (.NOT.ALLOCATED(atadc)) ALLOCATE (atadc(manz,manz))
+
+      
+      atadc=MATMUL(TRANSPOSE(sens),sens)
+      
+      
+      
+      IF (.NOT.ALLOCATED(inv_atadc)) 
+     1     ALLOCATE (inv_atadc(manz,manz))
+      
+      END SUBROUTINE bres_mat
