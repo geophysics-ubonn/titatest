bf41c830655f43fc7ede18a4d66c6a74b49d9ddd
Roland Martin <mail@rolandmartin.org>
2009-11-18 14:40:27 +0100

    -einfacheres plotten

--- m_tools/crtomo_plot.sh
+++ m_tools/crtomo_plot.sh
@@ -42,16 +42,6 @@ for x in $tools;do
 	i=$[$i+1];
 done
 
-awk '{if (NR==2){print}}' crtomo.cfg > tmp.meshname
-awk '{if (NR==3){print}}' crtomo.cfg > tmp.elecname
-echo $1 > tmp.fenster
-
-c1=`pwd|awk '/2_decades/{printf("%.1f\t%.1f",1.0,3.0)}'` 
-c2=`pwd|awk '/3_decades/{printf("%.1f\t%.1f",0.5,3.5)}'` 
-c3=`pwd|awk '/4_decades/{printf("%.1f\t%.1f",0.0,4.0)}'` 
-echo range check: $c1 $c2 $c3 $2
-echo $c1 $c2 $c3 $2 > tmp.range
-
 wdir=`pwd|xargs basename`
 
 if [ -n $1 ];then
@@ -62,16 +52,4 @@ cp $crt crtomo.$wdir
 
 ./crtomo.$wdir >& $wdir'.crtrun'
 
-$mtlb < $plotmod >& /dev/null
-
-tmp=`cat tmp.lastmod`
-myeps=`basename $tmp`'.eps'
-mypdf=`basename $myeps '.eps'`'.pdf'
-echo "making image conversion..."
-$epsp $myeps -o=$1$mypdf >& /dev/null
-$mogri -format jpg $myeps
-echo "done $1"
-
-#pdfcr=`which pdfcrop`
-#forcecheck $pdfcr
-#$pdfcr --margins 1 --hires $mypdf cropped.pdf >& /dev/null
+plot_cur_crtomo
