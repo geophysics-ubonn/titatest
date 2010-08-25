28299a2d55650e02fd7fddfb27cea9fb1365078f
Roland Martin <martin@geo.uni-bonn.de>
2010-08-25 09:25:45 +0200

    - still figuring out which way to plot results..

--- m_tools/stochastic_eval.sh
+++ m_tools/stochastic_eval.sh
@@ -38,11 +38,12 @@ pw='w p pt 1'
 lpw='w l lt 6 lw 2.5'
 psw='pointsize 0.5' # pointsize
 
+font='"Arial,18"' # general font and its size of the title and axes
 # set derived gnuplot variables
 term="postscript enhanced color font $font"
 #term="pngcairo enhanced color crop font $font"
 #key="inside bot horizontal font $lfw $lfws"
-key="at screen 0.98,0.98 horizontal font $lfw $lfws"
+key="at screen 0.98,0.98 Right horizontal font $lfw $lfws width 0.4 height 0.2"
 
 # multi
 # setting global gnuplot parameters 
@@ -61,10 +62,19 @@ echo 'set ytics nomirror' >> $tgr
 echo 'set ylab offset 1.5 "L_1 difference (1-x/true) [%]"' >> $tgr
 echo "set $psw" >> $tgr
 echo "set key $key"  >> $tgr
+
 echo 'set multiplot' >> $tgr
 echo 'set origin 0,0' >> $tgr
 echo 'set size 1.0,0.97' >> $tgr
 labscr='at screen 0.01,0.97'
+
+rms_glo=$cur/tmp.l1_rms_glo
+vario_glo=$cur/tmp.vario_glo
+vario_ver=$cur/tmp.vario_ver
+vario_hor=$cur/tmp.vario_hor
+rm -f $rms_glo $vario_glo $vario_ver $vario_hor
+
+
 for mode in $modes;do
 
     if [ -d $mode ];then
@@ -155,16 +165,31 @@ for mode in $modes;do
     minmax $fln_glob
     ymax=`echo $mmax|awk -v max=$ymaxv '{if($1>max){print max}else{print $1}}'`
     ymin=$mmin
+#
+    if [ $mode == "Exp" ];then
+	echo 'set size 1.0,0.33' > tmp.$mode
+	echo 'set label 1 "a) Exp" at screen 0.02,0.97' >> tmp.$mode
+	echo 'set origin 0,0.64' >> tmp.$mode
+    elif [ $mode == "Gau" ];then
+	echo 'unset key' > tmp.$mode
+	echo 'set size 1.0,0.31' >> tmp.$mode
+	echo 'set origin 0,0.33' >> tmp.$mode
+	echo 'set label 1 "b) Gau" at screen 0.02,0.65' >> tmp.$mode
+    elif [ $mode == "Sph" ];then
+	echo 'set label 1 "c) Sph" at screen 0.02,0.34' > tmp.$mode
+	echo 'set xlab offset 0,0.5 "Integral scale /[m]"' >> tmp.$mode
+    fi
 
 # global L1 fit and RMS plotting command
     fln=$fln_glob
-    tg1=$fln'.gnu'
+    tg1=tmp1
     out='"'$fln'.ps"'
     tit='"Global model fit and RMS ('$mode')"'
     
     echo "set out $out" > tmp.gnu
     echo "set label 1 $tit $labscr" >> tmp.gnu
-
+#    echo 'unset key' > $tg1
+#    echo 'set key inside left Left font "Arial,14"' $lfws >> $tg1  
     echo "set yrange[$ymin:$ymax]" > $tg1
     echo "set y2range [$y2min:$y2max]" >> $tg1
     echo 'plot \' >> $tg1
@@ -176,13 +201,16 @@ for mode in $modes;do
     echo '"'$fln_rms'" u 1:5 axes x1y2 '$lpw' lc 3 ti "(rms)",\' >> $tg1
     echo '"'$fln_glob'" u 1:6 '$lw' lc 4 ti "sph",\' >> $tg1
     echo '"'$fln_rms'" u 1:6 axes x1y2 '$lpw' lc 4 ti "(rms)"' >> $tg1
-    
-    cat $tgr $tg1 >> tmp.gnu
+
+    echo 'set size 1.0,0.92' > tmp
+
+    cat $tgr tmp $tg1 >> tmp.gnu
     gnuplot < tmp.gnu
+    cat tmp.$mode $tg1 >> $rms_glo
 
 # global variogram plotting commands
     fln=$fln_vr
-    tg1=$fln'.gnu'
+    tg1=tmp1
     out='"'$fln'.ps"'
     tit='"Global variogram fit ('$mode')"'
     
@@ -199,9 +227,10 @@ for mode in $modes;do
 
     cat $tgr $tg1 >> tmp.gnu
     gnuplot < tmp.gnu
+    cat tmp.$mode $tg1 >> $vario_glo
 
     fln=$fln_vh
-    tg1=$fln'.gnu'
+    tg1=tmp1
     out='"'$fln'.ps"'
     tit='"Horizontal variogram fit ('$mode')"'
     
@@ -217,9 +246,10 @@ for mode in $modes;do
     echo '"'$fln'" u 1:6 '$lw' lc 4 ti "sph"' >> $tg1
     cat $tgr $tg1 >> tmp.gnu
     gnuplot < tmp.gnu
+    cat tmp.$mode $tg1 >> $vario_ver
 
     fln=$fln_vv
-    tg1=$fln_vv'.gnu'
+    tg1=tmp1
     out='"'$fln'.ps"'
     tit='"Vertical variogram fit ('$mode')"'
     
@@ -235,64 +265,64 @@ for mode in $modes;do
     echo '"'$fln'" u 1:6 '$lw' lc 4 ti "sph"' >> $tg1
     cat $tgr $tg1 >> tmp.gnu
     gnuplot < tmp.gnu
+    cat tmp.$mode $tg1 >> $vario_hor
+
+#    rm tmp*
 
     cd $cur
 done
-exit
+# MULTIPLOTS!!
 #
-tg1='tmp.gnu' # plot file..
-# declare font size for the legends
-font='"Arial,14"' # general font and its size of the title and axes
-# major plots
-lw='w lp lt 1 lw 3.5'   # linetype and width of the major plots
-lfw='"Times,12"' # legend font
-lfw=$font
-lfws='spacing 0.8 samplen 0.4'
-pw='w p pt 1'
-lpw='w l lt 6 lw 2.5'
-psw='pointsize 0.5' # pointsize
-
+font='"Arial,18"' # general font and its size of the title and axes
 # set derived gnuplot variables
 term="postscript enhanced color font $font"
 #term="pngcairo enhanced color crop font $font"
 #key="inside bot horizontal font $lfw $lfws"
-key="at screen 0, screen 0 horizontal font $lfw $lfws"
+key="at screen 1,1 Right horizontal font $font $lfws width 0.3 height 0.2"
 
-# multi
-# setting global gnuplot parameters
-echo 'set st da l' > $tg1
-echo 'set grid' >> $tg1
-echo "set term $term" >> $tg1
-# for global model diff plot plot also the RMS
-echo 'set y2tics' >> $tg1
-echo 'set y2tics nomirror' >> $tg1
-echo 'set y2label offset -3 "RMS [%]"' >> $tg1
-echo 'set log y2' >> $tg1
+echo 'set st da l' > $tgr
+echo 'set grid' >> $tgr
+echo "set term $term" >> $tgr
 #
-echo 'set xtics nomirror' >> $tg1
-echo 'set xlab offset 0,0.5 "Integral scale /[m]"' >> $tg1
-echo 'set ytics nomirror' >> $tg1
-#echo 'set log y' >> $tg1
-echo 'set ylab offset 2 "L_1 difference (1-x/true) [%]"' >> $tg1
-echo "set $psw" >> $tg1
+echo 'set xtics nomirror' >> $tgr
+echo 'set ytics nomirror' >> $tgr
+echo 'set ylab offset 1.5 "(1-x/true) [%]"' >> $tgr
+echo "set $psw" >> $tgr
+echo "set key $key"  >> $tgr
 
-#
-
-
-
-tit="Global model fit and RMS ($mode)"
-tg2=$mode'_l1_glob_diff_rms'
-out='"'$tg2'.ps'
-
-echo 'set tit "'$tit'"' > $tg1
-echo "set out $out" >> $tg1
+echo 'set y2tics nomirror' > tmp1
+echo 'set y2label offset -1.5 "RMS [%]"' >> tmp1
+echo 'set log y2' >> tmp1
+echo 'set out "l1_rms_glob.ps"'  >> tmp1
+echo 'set origin 0,0' >> tmp1
+echo 'set multiplot layout 3,1' >> tmp1
+echo 'set log y' >> tmp1
 
 
+cat $tgr tmp1 $rms_glo > tmp
+gnuplot < tmp
 
-echo "set key $key"  >> $tg1
+
+echo 'set out "vario_glob.ps"'  > tmp1
+echo 'set origin 0,0' >> tmp1
+echo 'set multiplot layout 3,1' >> tmp1
+cat $tgr tmp1 $vario_glo > tmp
+gnuplot < tmp
 
 
+echo 'set out "vario_hor_ver.ps"'  > tmp1
+echo 'set origin 0,0' >> tmp1
+echo 'set multiplot layout 3,2' >> tmp1
+cat $tgr tmp1 $vario_hor $vario_ver > tmp
+gnuplot < tmp
 
+echo 'set out "vario_ver.ps"'  > tmp1
+echo 'set origin 0,0' >> tmp1
+echo 'set multiplot layout 3,1' >> tmp1
+cat $tgr tmp1 $vario_ver > tmp
+gnuplot < tmp
+
+exit
 
 #   echo 'set multiplot' >> $tg1
 #   echo 'set size 0.52,0.52' >> $tg1 # equal sizes..
