4025b0034767c9537c3c53a4d918ddd357fe3a8b
Roland Martin <martin@geo.uni-bonn.de>
2010-11-23 08:01:46 +0100

    - weiter im text...

--- m_tools/stochastic_condition_sim.sh
+++ m_tools/stochastic_condition_sim.sh
@@ -4,22 +4,51 @@ echo running $0 with PID $$ at `uname -n`
 date
 
 get_grid_info(){
-	regug=`awk '/Regular/{print}' $1`
-	echo "checking for a grid info.."
-	if [ -z "$regug" ];then
-		echo "there is no regular grid.."
-		exit
-	else
-		echo "found $regug"
-	fi
-	NX=`awk '{if($1=="NX"){print $2}}' $1`
-	X0=`awk '/X0/{print $2}' $1`
-	DX=`awk '/DX/{print $2}' $1`
-	NY=`awk '/NY/{print $2}' $1`
-	Y0=`awk '/Y0/{print $2}' $1`
-	DY=`awk '/DY/{print $2}' $1`
-	echo "NX:$NX X0:$X0 DX:$DX"
-	echo "NY:$NY Y0:$Y0 DY:$DY"
+    regug=`awk '/Regular/{print}' $1` # if the grid is a regular one, this is in the file..
+    echo "checking for a grid info.." 
+    if [ -z "$regug" ];then
+	echo "there is no regular grid.."
+	exit
+    else
+	echo "found $regug"
+    fi
+    NX=`awk '{if($1=="NX"){print $2}}' $1`
+    X0=`awk '/X0/{print $2}' $1`
+    DX=`awk '/DX/{print $2}' $1`
+    NY=`awk '/NY/{print $2}' $1`
+    Y0=`awk '/Y0/{print $2}' $1`
+    DY=`awk '/DY/{print $2}' $1`
+    echo "NX:$NX X0:$X0 DX:$DX" # grid settings, for linear equidistant grids
+    echo "NY:$NY Y0:$Y0 DY:$DY"
+}
+
+get_condition_data (){
+	echo "get condition data.."
+    if [ -e "$1" ];then
+	echo "$1 exists"
+    else
+	echo "$1 is no valid file"
+    fi
+    for x in $xlog;do
+	for y in $ylog;do
+	    echo "cheking value $x $y"
+	    val=`awk -v x=$x -v dx=$dxlog -v y=$y -v dy=$dylog -v m=$mean '{if((sqrt( ($1-x)^2 ) < dx) && (sqrt(($2-y)^2) < dy)) { print $1"  "$2"  "$3"  "$3-m } }' $1`
+	    if [ -n "$val" ];then
+		echo "found $val"
+		echo "$val" >> tmp.dat # remove mean from log values
+	    fi
+	done
+    done
+    n=`wc -l tmp.dat|awk '{print $1}'`
+    echo "found $n matches"
+    echo $n	> tmp.head
+    echo '4' >> tmp.head
+    echo 'x-coord' >> tmp.head
+    echo 'y-coord' >> tmp.head
+    echo 'resisitivity (log(Ohm m))' >> tmp.head
+    echo 'resisitivity-mean (log(Ohm m))' >> tmp.head
+    cat tmp.head tmp.dat > $2   
+    rm tmp.head tmp.dat
 }
 
 if [ -z "$1" ];then
@@ -31,11 +60,15 @@ else
 fi
 
 regud="true smo exp gau sph"
-regu=(0 0 15 15 15)
-xlog="2.5 7.25 12"
-dxlog=".2"
-ylog="-6"
-dylog="6"
+regu=(0 0 15 15 15) # regularization switches within crtomo.cfg
+# on place of nx
+xlog="2.5 7.25 12" # borehole log x-positions
+# may be got from inv.elecpositions..
+dxlog=".2" # delta x for the information range
+ylog="-6" # borehole y-positions
+dylog="6" # dy information range 
+mean=2.0  #log mean value for modelling-- calc from true.dat using my_meanvariance (grep MEAN) ?
+deca=3  # log decades as (maxi(log)-min(log)) -- from true.dat using my_minmax (grep DLOG) ?
 
 cur=`pwd`
 
@@ -51,13 +84,13 @@ else
 	get_grid_info ctmck.info
 	cd $cur
 fi
-exit
+
 mode=`basename $cur`
 
 if [ "$mode" == "Exp" ];then
-    echo "setting Exponential model"
-    rsto=(0 0 0 10 20)
-    it=1
+    echo "setting Exponential model" 
+    rsto=(0 0 0 10 20) # this controls the variogram settings for CRTomo, see crtomo.cfg from git repo
+    it=1 # variogram model for SGsim
 elif [ "$mode" == "Gau" ];then
     echo "setting Gauss model"
     rsto=(1 1 1 11 21)
@@ -69,6 +102,7 @@ elif [ "$mode" == "Sph" ];then
 else
     exit
 fi
+
 for x in *_$mode;do    
     ix=`echo $x|tr '_' ' '|awk '{print $2}'`
     iy=`echo $x|tr '_' ' '|awk '{print $4}'`
@@ -128,7 +162,7 @@ for x in *_$mode;do
 		./$mybin >& `echo $mybin|tr 'CRT' 'crt'`'.out'
 	    fi
 # plot ut the data
-	    plt_all_crt $z
+	    # plt_all_crt $z
 	    
 	    if [ "$z" == "true" ]; then
 # get x-vario fit parameter from true model
@@ -176,7 +210,7 @@ for x in *_$mode;do
         cd $cur/$x
     done
     cd $pub
-    plot_diff.sh
-    variogram.sh $ix $iy $mode
+    #plot_diff.sh
+    #variogram.sh $ix $iy $mode
     cd $cur
 done
