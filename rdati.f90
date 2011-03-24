e6b4b9204fc7cdd8017970c4eeafbd8a099551f9
Roland Martin <martin@geo.uni-bonn.de>
2011-03-24 13:28:55 +0100

    -to fit with testing version..

--- rdati.f90
+++ rdati.f90
@@ -243,7 +243,7 @@ subroutine rdati(kanal,datei)
 
            WRITE(ifp1,'(G14.4)')bet
            ! write out full noisy data as measured voltages..
-           WRITE (ifp3,*) strnr(i),vnr(i),bet,pha
+           WRITE (ifp3,*)strnr(i),vnr(i),bet,pha
         END IF
 
      end if
