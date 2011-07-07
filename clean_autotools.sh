87fb84c52b5458de466a3b6efb9f870699302050
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2011-07-07 16:00:21 +0200

    - still not working

---
+++ clean_autotools.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# bla
+
+# Die Makefiles werden beim Wechseln der branches überschrieben und gelöscht, also müssen eventuelle Objekte gelöscht werden
+rm src/*.o
+rm src/*.mod
+rm cutmckee/*.o
+rm cutmckee/*.mod
+
+# WICHTIG: Die Dateien sollten nur gelöscht werden, solange sie leer sind
+rm INSTALL
+rm COPYING
+rm NEWS
+rm README
+rm AUTHORS
+rm ChangeLog
+
+# crerror.h wird automatisch generiert
+rm inc/crerror.h
+rm cutmck/crerror.h
+
+# Lösche alle von autotools generierten Dateien
+
+# Autotools
+rm Makefile src/Makefile cutmckee/Makefile
+rm Makefile.in src/Makefile cutmckee/Makefile
+
+rm aclocal.m4
+
+rm config.guess
+rm config.log
+rm config.status
+rm config.sub
+
+rm configure
+
+rm install-sh
+rm libtool
+rm ltmain.sh
+rm missing
+test -z depcomp || rm depcomp
+
+rm -r autom4te.cache
+
+rm -rf package
