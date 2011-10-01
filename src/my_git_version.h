66c276307387c0a5acaa1c6598e58f4ef2df0322
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2011-10-01 22:02:10 +0200

    my_git_version.h now gets create automatically

--- src/my_git_version.h
+++
@@ -1,5 +0,0 @@
-CHARACTER (256) :: my_git_version(4)
-my_git_version(1) = "master-autotools"
-my_git_version(2) = "ca85fa5bee54ea5c3de295ec2393fecebdf6488b"
-my_git_version(3) = "Sat-Oct--1-21:59:52-2011"
-my_git_version(4) = ""
