114eb2c08cb440c68a7b3ce280e70283103aaba9
Roland Martin <martin@geo.uni-bonn.de>
2012-02-01 08:29:04 +0100

    - Checks for Operating System are undertaken

--- get_git_version.sh
+++ get_git_version.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "CHARACTER (256) :: my_git_version(4)" > my_git_version.h
-commit=`git log | head -1|awk '{print $2}'`
-branch=`git branch|awk '/\*/{print $2}'`
-mydate=`date +%c|sed 's/\ /-/g'`
+# get the character specification
+grep "CHARACTER (256)" get_git_ver.f90 | sed 's/,PUBLIC//g' | sed 's/version/my_git_version/g' > my_git_version.h
+commit=$( git log | head -1 | awk '{print $2}' )
+branch=$( git branch | awk '/\*/{print $2}' )
+mydate=$( date +%c | sed 's/\ /-/g' )
 compiler=$1
+myos=$( uname -o )
 echo 'my_git_version(1) = "'$branch'"' >> my_git_version.h
 echo 'my_git_version(2) = "'$commit'"' >> my_git_version.h
 echo 'my_git_version(3) = "'$mydate'"' >> my_git_version.h
 echo 'my_git_version(4) = "'$compiler'"' >> my_git_version.h
+echo 'my_git_version(5) = "'$myos'"' >> my_git_version.h
