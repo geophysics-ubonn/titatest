8668359165ce017efebe7fdac8534c32809ac78a
Roland Martin <martin@geo.uni-bonn.de>
2013-09-04 11:44:32 +0200

    - ADD: some more funcionality to doxygen stuff, including automated refman.pdf and so on

--- scripts/make_doxygen.sh
+++ scripts/make_doxygen.sh
@@ -12,8 +12,10 @@ tag="$1"
 awk -v tag=$tag '{if($1=="PROJECT_NAME"){print $1" = "tag}else{print}}' doxy.inp > tmp.inp
 tag=$branch
 awk -v tag=$tag '{if($1=="PROJECT_NUMBER"){print $1" = "tag}else{print}}' tmp.inp > doxy.inp
+outdir="$2"
+awk -v tag=$outdir '{if($1=="OUTPUT_DIRECTORY"){print $1" = "tag}else{print}}' tmp.inp > doxy.inp
 
-my_list_of_values="OPTIMIZE_FOR_FORTRAN EXTRACT_ALL GENERATE_TREE_VIEW"
+my_list_of_values="OPTIMIZE_FOR_FORTRAN EXTRACT_ALL GENERATE_TREE_VIEW HAVE_DOT CALL_GRAPH CALLER_GRAPH GENERATE_LATEX USE_PDFLATEX PDF_HYPERLINKS LATEX_SOURCE_CODE GENERATE_DOCBOOK GENERATE_TREEVIEW"
 for member in $my_list_of_values;do
 
 	awk -v member=$member '{if($1==member){print $1" = Yes"}else{print}}' doxy.inp > tmp.inp
@@ -21,3 +23,5 @@ for member in $my_list_of_values;do
 
 done
 doxygen doxy.inp
+cd $outdir/latex
+make
