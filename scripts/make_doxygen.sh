d38a06db5a32412d7654ef2a4bf7da61e8459b32
Roland Martin <martin@geo.uni-bonn.de>
2013-09-16 11:19:37 +0200

    - FIX: wrong brach ids from overwriting tmp.inp twice

--- scripts/make_doxygen.sh
+++ scripts/make_doxygen.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-commit=`git log | head -1|awk '{print $2}'`
-branch=`git branch|awk '/\*/{print $2}'`
-mydate=`date +%c|sed 's/\ /-/g'`
+commit=$(git log | head -1|awk '{print $2}')
+branch=$(git branch|awk '/\*/{print $2}')
+mydate=$(date +%c|sed 's/\ /-/g')
 
-if [ -e doxy.inp ];then
-	echo doxy.inp exists.
+if [ -f dox.inp ];then
+	echo "taking existent doxy.inp"
 else
+	echo "creating default doxy input file"
 	doxygen -g doxy.inp
 fi
-tag="$1"
-awk -v tag=$tag '{if($1=="PROJECT_NAME"){print $1" = "tag}else{print}}' doxy.inp > tmp.inp
-tag=$branch
-awk -v tag=$tag '{if($1=="PROJECT_NUMBER"){print $1" = "tag}else{print}}' tmp.inp > doxy.inp
-outdir="$2"
-awk -v tag=$outdir '{if($1=="OUTPUT_DIRECTORY"){print $1" = "tag}else{print}}' tmp.inp > doxy.inp
 
+tag1="$1"
+tag2="$branch"
+tag3="$2"
+echo "edit PROJECT_NAME = $tag1"
+echo "edit PROJECT_NUMBER = $tag2"
+echo "edit OUTPUT_DIR = $tag3"
+awk -v t1="$tag1" -v t2="$tag2" -v t3="$tag3" '{ 
+						if($1 == "PROJECT_NAME" ){print $1" = "t1}
+						else if($1 == "PROJECT_NUMBER" ){print $1" = "t2}
+						else if($1 == "OUTPUT_DIR" ){print $1" = "t3}
+						else{print}
+						}' doxy.inp > tmp.inp
+
+cp tmp.inp dox.inp
 my_list_of_values="OPTIMIZE_FOR_FORTRAN EXTRACT_ALL GENERATE_TREE_VIEW HAVE_DOT CALL_GRAPH CALLER_GRAPH GENERATE_LATEX USE_PDFLATEX PDF_HYPERLINKS LATEX_SOURCE_CODE GENERATE_DOCBOOK GENERATE_TREEVIEW"
+#my_list_of_values="OPTIMIZE_FOR_FORTRAN EXTRACT_ALL GENERATE_TREE_VIEW HAVE_DOT CALL_GRAPH CALLER_GRAPH GENERATE_LATEX USE_PDFLATEX PDF_HYPERLINKS LATEX_SOURCE_CODE GENERATE_DOCBOOK GENERATE_TREEVIEW"
 for member in $my_list_of_values;do
 
 	awk -v member=$member '{if($1==member){print $1" = Yes"}else{print}}' doxy.inp > tmp.inp
@@ -23,5 +33,5 @@ for member in $my_list_of_values;do
 
 done
 doxygen doxy.inp
-cd $outdir/latex
+cd $tag3/latex
 make
