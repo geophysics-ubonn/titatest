4c1a654d4bd949e2ac57f543078a4de13c069178
Johannes Kenkel <kenkel@geo.uni-bonn.de>
2014-09-15 12:03:27 +0200

    Fixed comp_ab: switch off old matrix assign

--- src/comp_ab.f90
+++ src/comp_ab.f90
@@ -91,10 +91,8 @@ subroutine comp_ab(ki,my_a_mat_band,nelec)
                     dum  = relbg(rel,ikl) * kg(rel,nelec,ki) 
                     dum2 = cmplx(dum)*sigma(rnr(rel))
                  ! band matrix index (see above)
-                 dum=real(sum(my_a_mat_band))
-                call assign_zgbsvx(my_a_mat_band,nnp,nzp,mb,sanz,dum2)
+!                call assign_zgbsvx(my_a_mat_band,nnp,nzp,mb,sanz,dum2)
                 call assign_zpbsvx(my_a_mat_band,nnp,nzp,mb,sanz,dum2)
-!                print*,dum-sum(my_a_mat_band)
                 end if
                  end if
            end do
