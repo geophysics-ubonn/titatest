bad3c4e2709e48a5014ea7de258cf06ea691625e
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2016-11-27 12:16:00 +0100

    more work on MGS examples/characterization

--- examples/MGS/plot_invs.py
+++ examples/MGS/plot_invs.py
@@ -12,50 +12,53 @@ td = TD.tomodir()
 # CRTomo.config is a dictionary with additional functions
 import crlab_py.CRTomo as CRTomo
 import glob
+from multiprocessing import Pool, TimeoutError
 
 
-def create_overview(prefix):
+def plot_td(directory):
     summary_dir = 'overview'
+    outfile = summary_dir + '/' + os.path.basename(directory) + '.png'
+    if os.path.isfile(outfile):
+        return
 
-    if(not os.path.isdir(summary_dir)):
+    if not os.path.isdir(summary_dir):
         os.makedirs(summary_dir)
+    if not os.path.isdir(directory):
+        return
+    print(directory)
 
-    for directory in sorted(glob.glob('SIMS/*')):
-        if not os.path.isdir(directory):
-            continue
-        print(directory)
+    # plot
+    pwd = os.getcwd()
+    os.chdir(directory)
+    cmd = ''.join((
+        'plot_td.py ',
+        ' --cminmag 20 --cmaxmag 50 ',
+        ' --cminpha -30 --cmaxpha 0 ',
+        ' --cminim -4 --cmaxim -3 --cminre -1.5 --cmaxre -1.2',
+        ' --cmaglin ',
+    ))
+    subprocess.call(cmd, shell=True)
+    subprocess.call('td_create_overview.py', shell=True)
+    os.chdir(pwd)
 
-        # plot
-        pwd = os.getcwd()
-        os.chdir(directory)
-        cmd = ''.join((
-            'plot_td.py ',
-            ' --cminmag 20 --cmaxmag 50 ',
-            ' --cminpha -30 --cmaxpha 0 ',
-            ' --cminim -4 --cmaxim -3 --cminre -1.5 --cmaxre -1.2',
-            ' --cmaglin ',
-        ))
-        subprocess.call(cmd, shell=True)
-        subprocess.call('td_create_overview.py', shell=True)
-        os.chdir(pwd)
+    ## copy overview ##
+    overview_file = directory + os.sep + 'overview.png'
+    if(os.path.isfile(overview_file)):
+        os.system('cp {0} {1}'.format(overview_file, outfile))
 
-        ## copy overview ##
-        overview_file = directory + os.sep + 'overview.png'
-        if(os.path.isfile(overview_file)):
-            outfile = summary_dir + '/' + os.path.basename(directory) + '.png'
-            os.system('cp {0} {1}'.format(overview_file, outfile))
 
-        # ## copy 10 Hz ##
-        # tdir = directory + os.sep + 'invmod/05_10.000000'
-        # cmplx, fpi = td.get_result_files(tdir)
+def create_overview(indir, outdir):
+    pool = Pool(processes=4)
 
-        # for obj, name in ((cmplx.cre, 'cre'),
-        #                   (cmplx.cim, 'cim')):
-        #     in_mag_file = obj + '.png'
-        #     out_mag_file = single_dir + os.sep + name
-        #     out_mag_file += directory + '_10Hz.png'
-        #     os.system('cp "{0}" "{1}"'.format(in_mag_file, out_mag_file))
+    directories = sorted(glob.glob('{0}/*'.format(indir)))
+    pool.map(plot_td, directories)
+    # plots = map(plot_td, directories)
+    # for p in plots:
+    #     print(p)
+    # for directory in directories:
+    #     plot_td(directory)
 
 
 if __name__ == '__main__':
-    create_overview('parstudy_')
+    # create_overview('SIMS', 'output_SIMS')
+    create_overview('SIM2', 'output_SIMS2')
--- examples/MGS/run.py
+++ examples/MGS/run.py
@@ -17,9 +17,11 @@ def create_sim(outdir, mgs_type, mgs_beta):
     cfg['cells_z'] = '-1'
     cfg['fpi_inv'] = 'F'
 
-    cfg['mag_rel'] = '1'
+    # cfg['mag_rel'] = '1'
+    # cfg['mag_abs'] = '5e-4'
+    # cfg['pha_abs'] = '0'
+    cfg['mag_rel'] = '0.5'
     cfg['mag_abs'] = '5e-4'
-
     cfg['pha_abs'] = '0'
 
     cfg['d2_5'] = '1'
@@ -31,10 +33,10 @@ def create_sim(outdir, mgs_type, mgs_beta):
     cfg.write_to_file(outdir + os.sep + 'exe/crtomo.cfg')
     print(cfg)
 
-    shutil.copy('GRID/config.dat', outdir + os.sep + 'config/config.dat')
-    shutil.copy('GRID/elem.dat', outdir + os.sep + 'grid/elem.dat')
-    shutil.copy('GRID/elec.dat', outdir + os.sep + 'grid/elec.dat')
-    shutil.copy('GRID/rho.dat', outdir + os.sep + 'rho/rho.dat')
+    shutil.copy('GRID2/config.dat', outdir + os.sep + 'config/config.dat')
+    shutil.copy('GRID2/elem.dat', outdir + os.sep + 'grid/elem.dat')
+    shutil.copy('GRID2/elec.dat', outdir + os.sep + 'grid/elec.dat')
+    shutil.copy('GRID2/rho4.dat', outdir + os.sep + 'rho/rho.dat')
 
     pwd = os.getcwd()
     os.chdir(outdir + os.sep + 'exe')
@@ -47,7 +49,8 @@ def create_sim(outdir, mgs_type, mgs_beta):
         # seed
         fid.write('1\n')
         # a = 1%
-        fid.write('1\n')
+        # fid.write('1\n')
+        fid.write('0.4\n')
         fid.write('1e-4\n')
         fid.write('0\n')
         fid.write('0\n')
@@ -55,20 +58,32 @@ def create_sim(outdir, mgs_type, mgs_beta):
         fid.write('0\n')
 
 
-if __name__ == '__main__':
+def prepare_simulation(simdir):
+
     mgs_betas = np.hstack((
         np.logspace(-5, -1, 5),
-        0.5, 0.8, 0.9, 1.0,
+        0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.48,
+        0.5,
+        0.51, 0.55,
+        0.7, 0.71, 0.72, 0.73, 0.74, 0.75,
+        0.8, 0.85, 0.9, 1.0,
     ))
-    for mgs_type in list(range(5, 10)):
+    for mgs_type in (7, ):
         for mgs_beta in mgs_betas:
-            directory = 'SIMS/mgs_type_{0}_beta_{1}'.format(mgs_type, mgs_beta)
+            directory = '{0}/mgs_type_{1}_beta_{2:.8f}'.format(
+                simdir,
+                mgs_type,
+                mgs_beta
+            )
             if not os.path.isdir(directory):
                 os.makedirs(directory)
                 create_sim(directory, mgs_type, mgs_beta)
 
     # we need ine reference unversion using the regular triangular reg.
-    directory = 'SIMS/smoothness_regularization'
+    directory = simdir + '/smoothness_regularization'
     if not os.path.isdir(directory):
         os.makedirs(directory)
         create_sim(directory, 1, 0)
+
+if __name__ == '__main__':
+    prepare_simulation('SIM2')
