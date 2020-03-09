78437438598f65e675c4aa7f04ef85aec56dc5aa
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-03-09 07:38:55 +0100

    autogen.sh: do not overwrite AUTHORS file

--- autogen.sh
+++ autogen.sh
@@ -17,12 +17,6 @@ cd src
 ../scripts/get_git_version.sh
 cd ..
 
-# create AUTHORS file
-echo "AK - A. Kemna (kemna@geo.uni-bonn.de)" > AUTHORS
-echo "JK - J. Kenkel (jkenkel@geo.uni-bonn.de)" >> AUTHORS
-echo "RM - R. Martin (rmartin@geo.uni-bonn.de)" >> AUTHORS
-echo "MW - M. Weigand (mweigand@geo.uni-bonn.de)" >> AUTHORS
-
 touch NEWS ChangeLog
 
 echo -n "aclocal... "
